275fdf25b572cdff177bff98e82fbe6e93ae933b
dpletzke <dpletzke@gmail.com>
2022-05-22 16:41:10 -0500

    remove declarations

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,3 @@
-import { useEffect, useRef } from "react";
-import esriConfig from "@arcgis/core/config";
-import styled from "@emotion/styled";
-
-const MapContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 0px;
-  margin: 0px;
-  overflow: hidden;
-`;
-
 const Home = () => {
   return (
     <>
